a319af3c3f50bb8e7ed84f38371db036a82690d5
EC2 Default User <ec2-user@ip-172-31-60-187.ec2.internal>
2021-05-09 12:19:42 +0000

    [review　１〜６]

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,20 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  
+
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    # この記述を「ネストする」という
+    # このようにコーディングすることでどのbookに対してのいいねやコメントなのかを紐づける事ができる
+    # 複数形と単数形の違いは情報が複数か一つしかないかの違い
+    # resourceの違いに関してはhttps://qiita.com/wacker8818/items/1ba526fcbc73e065a511を参照すると良い
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:index, :show, :edit, :update] do
   resource :relationships, only: [:create, :destroy]
   	get 'followings' => 'relationships#followings', as: 'followings'
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
+
 end
